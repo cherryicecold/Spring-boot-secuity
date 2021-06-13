4bc13aa95ffa64ae152a44f06f2359da10a39af7
Karanpreet Bhandari <karanpreet2494@gmail.com>
2021-06-13 23:59:49 +0530

    added password encoder

--- src/main/java/com/example/secuitydemo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/secuitydemo/security/ApplicationSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.secuitydemo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,11 +9,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -29,10 +38,16 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     protected UserDetailsService userDetailsService() {
         UserDetails roles1 = User.builder()
-                .username("annasmith")
-                .password("password")
-                .roles("STUDENT") //ROLE_STUDENT
+                .username("karan")
+                .password(passwordEncoder.encode("bhandari"))
+                .roles("ADMIN") //ROLE_STUDENT
                 .build();
-        return new InMemoryUserDetailsManager(roles1);
+
+        UserDetails roles2 = User.builder()
+                .username("annasmith")
+                .password(passwordEncoder.encode("password12"))
+                .roles("STUDENT")
+                .build();
+        return new InMemoryUserDetailsManager(roles1,roles2);
     }
 }
