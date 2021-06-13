a21a320239c48d3b1c5be366ca8415e1f3a1b890
Karanpreet Bhandari <karanpreet2494@gmail.com>
2021-06-13 17:39:51 +0530

    added basic auth security

---
+++ src/main/java/com/example/secuitydemo/SecuitydemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.secuitydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuitydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuitydemoApplication.class, args);
+	}
+
+}
